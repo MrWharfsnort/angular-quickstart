31b837957aef1b20f3f8654eab337780ed59406a
Nate Farnsworth <nfarnsworth@gmail.com>
2016-10-27 16:12:54 -0600

    :tada: initial commit

---
+++ app/app.component.ts
@@ -0,0 +1,27 @@
+import { Component } from '@angular/core';
+
+
+@Component({
+    selector: 'my-app',
+    template: `<h1>Hello World</h1>`
+})
+export class AppComponent {
+
+    // public secondsPassed: number = 0;
+    // public name: string = "";
+    // public cats: number = 0;
+
+    // ngOnInit() {
+    // 	setInterval(() => {
+    // 		this.secondsPassed ++;
+    // 	}, 1000);
+    // }
+
+    // onTextChange(evt) {
+    // 	this.name = evt.target.value.toUpperCase();
+    // }
+
+    // sayHi() {
+    // 	alert("Hey!");
+    // }
+}
